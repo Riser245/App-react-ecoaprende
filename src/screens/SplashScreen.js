894005f4faea1e36e43ed180efe38bd7db0b56b4
jm0004 <20190148@ricaldone.edu.sv>
2024-07-11 22:36:17 -0600

    Arreglo splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,41 +1,52 @@
 import React, { useEffect } from 'react';
 import { Video } from 'expo-av';
 import { StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 
 const SplashScreenComponent = () => {
-    useEffect(() => {
-        const splashTimer = setTimeout(() => SplashScreen.hide(), 5000); // Ocultar el splash después de 5 segundos
-        return () => clearTimeout(splashTimer);
-    }, []);
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
 
-    return (
-        <View style={styles.container}>
-            <Video
-                source={require('../../assets/SplashVideo.mp4')} // Ruta al archivo de video
-                rate={1.0}
-                isMuted={true}
-                resizeMode="cover"
-                shouldPlay
-                isLooping
-                style={styles.video}
-            />
-        </View>
-    );
+    const splashTimer = setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 5000);
+
+    return () => clearTimeout(splashTimer);
+  }, []);
+
+  const handleError = (error) => {
+    console.error("Error loading video: ", error);
+    SplashScreen.hideAsync();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Video
+        source={require('../../assets/SplashScreen.png')}
+        rate={1.0}
+        isMuted={true}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={styles.video}
+        onError={handleError}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    video: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  video: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
 });
 
 export default SplashScreenComponent;
